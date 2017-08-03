114a032966bcc4a98567648a5a3be7cdf88d7aa1
rudiantotiofan <rudiantotiofan@gmail.com>
2017-08-03 10:45:25 +0700

    Abstract Source

---
+++ oop/abstraction/abstract.php
@@ -0,0 +1,23 @@
+<?php 
+abstract class AbstractClass
+{
+    abstract protected function prefixName($name);
+}
+
+class ConcereteClass extends AbstractClass
+{
+    public function prefixName($name, $separator = "."){
+        if($name == "Pacman"){
+            $prefix = "Mr";
+        }elseif ($name == "Pacwoman"){
+            $prefix = "Mrs";
+        }else {
+            $prefix = "";
+        }
+        return "{$prefix}{$separator}{$name}";
+    }
+}
+$class = new ConcereteClass;
+echo $class->prefixName("Pacman"),"\n";
+echo $class->prefixName("Pacwoman"),"\n";
+?>
