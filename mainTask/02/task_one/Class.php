955d199e9ef0cbca0e414ce03335b4135ccf5adf
rudiantotiofan <rudiantotiofan@gmail.com>
2017-08-10 15:55:01 +0700

    update mainTask 02 laravel

--- mainTask/02/task_one/Class.php
+++ mainTask/02/task_one/Class.php
@@ -7,13 +7,78 @@ class mainClass{
     }
     public function nPlayerSet($player){
        array_push($this->player,$player);  //jika array tidak kosong makan push array
+    }
+    public function updateStatus($player){
+       foreach($this->player as $key=>$val)
+        {
+            if($this->player[$key]['name']==$player['name']){
+                $this->player[$key]['mana'] = $player['mana'];
+                $this->player[$key]['blood'] = $player['blood'];
+                break;
+            }
+        }
     }  
     public function validateInput($input){
-        if(!isset($input)){
-            throw new exception("Input tidak boleh kosong !, silahkan tekan sembarang tombol.");
-            fgets(STDIN);
+        if(empty($input)){
+            throw new exception("Input can't be empty !, Press any button.");
         }
         return $input;
+    }
+    public function validatePlayerName($input){
+        foreach($this->player as $val)
+        {
+            if($val['name']==$input){
+                throw new exception("Player already exist !, Press any button.");
+                break;
+            }
+        }
+        return $input;
+    }
+    public function validateExistPlayer($input){
+        $check = false;
+        foreach($this->player as $val)
+        {
+            if($val['name']==$input || $input=="exit"){
+                $check = true;
+                if($val['mana']==0 || $val['blood']==0){
+                    throw new exception("Player is dead, choose another player !, Press any button.");
+                }
+            }
+        }
+        if($check==false){
+            throw new exception("Player not found !, Press any button.");
+        }else{return $input;}
+    }
+    public function survivePlayer(){
+        $count = 0;
+        foreach($this->player as $val)
+        {
+            if(($val['mana']>0) && ($val['blood']>0)){
+                $count++;
+            }
+        }
+        return $count;
+    }
+    public function winnerPlayer(){
+        foreach($this->player as $val)
+        {
+            if(($val['mana']>0) && ($val['blood']>0)){
+                return $val['name'];
+            }
+        }
+        return "not fond";
+    }
+    public function isDeath($player){
+        foreach($this->player as $val)
+        {
+            if($val['name']==$player['name']){
+                if(($val['mana']==0) || ($val['blood']==0)){
+                    return true;
+                    break;
+                }
+            }
+        }
+        return false;
     }
     public function disHome(){
         $this->cls();
@@ -45,15 +110,50 @@ class mainClass{
         echo "=====================================\n";
         echo "    Welcome to the Battel Arena\n";
         echo "-------------------------------------\n";
-        echo "Descripotion :\n";
-        echo "1. type \"new\" to create a character\n";
-        echo "2. type \"start\" to begin the fight \n";
+        echo "Player Exist : \n";
+        if(count($this->player)>0){
+            foreach($this->player as $val)
+            {
+            echo "- ".$val['name']."\n";
+            }
+        }else{
+            echo "-#\n";    
+        }
         echo "-------------------------------------\n";
         echo "Put Player Name :";
-        echo "\n-\n";
-        echo "*Max player 2 or 3\n";
         $input = trim(fgets(STDIN));
         return $input;
+    }
+    public function disBattle($name){
+        $this->cls();
+        echo "=====================================\n";
+        echo "    Welcome to the Battel Arena\n";
+        echo "-------------------------------------\n";
+        echo "Player Status : \n";
+        if(count($this->player)>0){
+            foreach($this->player as $val)
+            {
+                if(($val['mana']==0) || ($val['blood']==0)){ $stat = " (Your Dead)";}else{ $stat = "";}
+                echo "- [".$val['name']."] : mana = ".$val['mana'].", blood = ".$val['blood']."".$stat."\n";
+            }
+            echo "-------------------------------------\n";
+            echo "Player role : ".$name."\n";
+            echo "-------------------------------------\n";
+            echo "Attack player  : ";
+        }else{
+            echo "- No character found !, Press any button \n";    
+        }
+        return trim(fgets(STDIN));
+    }
+    public function dispLogAttack($info){
+        echo "-------------------------------------\n";
+        echo "Attack log :\n";
+        echo " Basic Demage : ".$info['basic']."\n";
+        echo " Miss  Demage : ".$info['miss']."\n";
+        echo " Crit  Demage : ".(($info['crit']/$info['basic'])*100)." % \n";
+        echo "-------------------------------------\n";
+        echo "Press any button to continue \n";    
+        trim(fgets(STDIN));
     }
 }
 
@@ -67,17 +167,28 @@ class player{
         $this->mana = $mana;
         $this->blood = $blood;
     }
+    public function reset($mana,$blood){
+        $this->mana = $mana;
+        $this->blood = $blood;
+    }
     public function setName($name){ $this->name = $name;}   //untuk mengambil set nama player
     public function getName(){ return $this->name;}    //untuk mengambil get nama player
     public function setMana($mana){ $this->mana = $mana;}   //untuk mengambil set nama player
     public function getMana(){ return $this->mana;}    //untuk mengambil get mana player
     public function setBlood($blood){ $this->blood = $blood;}   //untuk mengambil set nama player
     public function getBlood(){ return $this->blood;}  //untuk mengambil get hp player    
-    public function decreaseMana(){
-        $this->mana = $this->mana-10;
+    public function getPlayerStat(){
+        return array("name"=>$this->name,"mana"=>$this->mana,"blood"=>$this->blood);
     }
-    public function decreaseBlood(){
-        $this->blood = $this->blood-25;
+    public function decreaseBlood($blood){
+        $this->blood = $this->blood - $blood;
+        if($this->blood < 0){$this->blood = 0;}
     }
+    public function demageAttack(){
+        $this->mana = $this->mana-5;
+        if($this->mana < 0){$this->mana = 0;}
+        return array("basic"=>20,"crit"=>rand(0,10),"miss"=>rand(0,1));
+    }
+    
 }
 ?>
--- mainTask/02/task_one/Main.php
+++ mainTask/02/task_one/Main.php
@@ -2,30 +2,74 @@
 include("Class.php");
 $main = new mainClass();
 $player;
-$pushPlayer = function($name,$mp,$blood){ return array("name"=>$name,"mp"=>$mp,"blood"=>$blood);};
+//$pushPlayer = function($name,$mp,$blood){ return array("name"=>$name,"mp"=>$mp,"blood"=>$blood);};
 $counter = 1;
 while($input!="exit"){
+home:    
 $input = $main->disHome();
     switch($input){
-        case "new": 
-            labelNew :
-            $playerName = $main->disNewChar();
+        case "new":
+            $playerName = $main->disNewChar(); //get player display
             try{    
                 if($counter>3){
-                    throw new exception("maksimum hanya 3 player saja !, silahkan tekan sembarang tombol.");
-                    fgets(STDIN);
+                    throw new exception("has reach maximum slot !, Press any button.");
                 }else{
-                    $playerName = $main->validateInput($playerName);
+                    $playerName = $main->validatePlayerName(strtolower($main->validateInput($playerName))); //check empty string and exist player name
                     $player[$playerName] = new player($playerName,40,100);
-                    $main->nPlayerSet($pushPlayer($player[$playerName]->getName(),$player[$playerName]->getMana(),$player[$playerName]->getBlood()));
+                    $main->nPlayerSet($player[$playerName]->getPlayerStat());
                     $counter++;
-                    fgets(STDIN);
                 }
             }catch(exception $e){
-                echo $e->getMessage();
+                echo PHP_EOL."Warning : ".$e->getMessage();
+                fgets(STDIN);
             }
             break;
-        case "start": break;
+        case "start": 
+            try{
+                if(count($main->player)<1){
+                    throw new exception("create new player first !, Press any button.");
+                }else{
+                    while($main->survivePlayer()>1){
+                        foreach($main->player as $val){
+                            try{
+                                batle :
+                                if($main->isDeath($player[$val['name']]->getPlayerStat())){ //check player stat
+                                    //do nothing, just skip the loser
+                                }else{ //if player not dead
+                                    $input = $main->validateExistPlayer($main->validateInput($main->disBattle($val['name']))); //check empty string and empty player choosen
+                                    if($input==$val['name']){
+                                        throw new exception("Cant attack your self ! Press any button");
+                                    }else{
+                                        $demage = $player[$val['name']]->demageAttack(); //get demage from player
+                                        $player[$input]->decreaseBlood(($demage['basic']+$demage['crit'])-$demage['miss']);
+                                        $main->updateStatus($player[$val['name']]->getPlayerStat()); //update status player
+                                        $main->updateStatus($player[$input]->getPlayerStat()); //update status enemy
+                                        $main->dispLogAttack($demage);
+                                    }
+                                }
+                            }catch(exception $e){
+                                echo PHP_EOL."Warning : ".$e->getMessage();
+                                fgets(STDIN);
+                                goto batle;
+                            }
+                            if($main->survivePlayer()==1){
+                                echo "The one survive is ".$main->winnerPlayer();
+                                fgets(STDIN);
+                                }
+                            if($input=="exit"){goto home; break;}
+                        }                
+                    }//endwhile
+                }
+
+            }catch(exception $e){
+                echo PHP_EOL."Warning : ".$e->getMessage();
+                fgets(STDIN);
+            }
+            break;
+        case "exit": break;
+        default :
+            echo PHP_EOL."Undefiend command !, Press any button";
+            fgets(STDIN);
     }
 }
 
